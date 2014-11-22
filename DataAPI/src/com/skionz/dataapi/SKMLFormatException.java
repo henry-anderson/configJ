1e85e88fc0b7274e239d4e462f20867ba5b57488
Skionz <henryanderson1@hotmail.com>
2014-11-21 19:13:59 -0600

    Renamed the class

---
+++ DataAPI/src/com/skionz/dataapi/SKMLFormatException.java
@@ -0,0 +1,21 @@
+package com.skionz.dataapi;
+
+public class SKMLFormatException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public SKMLFormatException() {
+		super();
+	}
+
+	public SKMLFormatException(String message) {
+		super(message);
+	}
+
+	public SKMLFormatException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public SKMLFormatException(Throwable cause) {
+		super(cause);
+	}
+}
